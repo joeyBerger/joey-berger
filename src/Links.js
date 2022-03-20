40f3b74f410ca44999359fb417de5c3b266637a2
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2022-03-20 16:51:09 -0700

    added link comp

---
+++ src/Links.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Col } from 'react-bootstrap';
+import {linkImages,rows} from './LinksImages'
+
+const Links = () => {
+    
+    const openTab = (e,link) => {
+        e.preventDefault()
+        window.open(link, "_blank");
+    }
+
+    return (
+        <>
+            {rows.map((row,i) => 
+                <Col md={{ span: 11, offset: 2 }} className='d-flex justify-content-start' style={i ? {marginTop:'40px'} : {}}>
+                    {row.map(img => <img src={img} alt={linkImages[img].alt} onClick={(e) => {openTab(e,linkImages[img].link)}}></img>)}
+                </Col>                
+            )}
+        </>
+    )
+}
+
+export default Links
