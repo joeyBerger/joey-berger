c1b52a332c886a52f8d9ca941dabc743327eb88a
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2022-03-20 17:26:57 -0700

    split to more components

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Contact from './Contact'
 import { landingPageContent } from './LandingPageContent';
 import ReactPlayer from "react-player"
 import { isMobile } from 'react-device-detect';
+import Landing from './Landing'
+import CustomNavbar from './CustomNavbar';
 import Stacks from './Stacks';
 import Links from './Links'
 
@@ -19,7 +21,7 @@ class MainContent extends React.Component {
         const width = isMobile ? window.innerWidth * .7 : 500
         this.reactPlayerDimensions = {
             width : `${width}px`,
-            height : width*.75//'400px'
+            height : width*.75,
         }
     }    
 
@@ -40,75 +42,32 @@ class MainContent extends React.Component {
         this.setState(() => ({expanded}))
     }
 
+    categories = {
+        software : 'Software Engineer',
+        sound : 'Composition/Sound Design',
+        guitar : 'Guitarist',
+        links : 'Links',
+        stacks : 'Stacks/Languages',
+        contact : 'Contact',
+    }
+
     render() {        
         return(
         <div className="container-xs">
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top" expanded={this.state.expanded}>
-            <Navbar.Brand>Joey Berger</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={(e) => this.setNavBarExpanded(e,!this.state.expanded)}/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                </Nav>
-                <Nav>      
-                <Nav.Link onClick = {(e) => {this.scrollToDiv(e,"software")}}>
-                    Software Engineer
-                </Nav.Link> 
-                <Nav.Link onClick = {(e) => {this.scrollToDiv(e,"sound")}}>
-                    Composition/Sound Design
-                </Nav.Link> 
-                <Nav.Link onClick = {(e) => {this.scrollToDiv(e,"guitar")}}>
-                    Guitarist
-                </Nav.Link>
-                <Nav.Link onClick = {(e) => {this.scrollToDiv(e,"links")}}>
-                    Links
-                </Nav.Link>
-                <Nav.Link onClick = {(e) => {this.scrollToDiv(e,"stacks")}}>
-                    Stacks/Languages
-                </Nav.Link>
-                <Nav.Link onClick = {(e) => {this.scrollToDiv(e,"contact")}}>
-                    Contact
-                </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Navbar>
-            <div className="image-container">
-                <div className = {`background-img ${isMobile ? 'mobile-source' : ''} fade-in`}></div>
-                <Container className="centered">
-                    <Row>
-                        <Col sm={12} lg={12} className="d-none d-xl-block centered">
-                        Joey Berger
-                        <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
-                        </Col>
-                        <Col sm={12} lg={12} className="d-none d-lg-block d-xl-none centered-lg">
-                        Joey Berger
-                        <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
-                        </Col>
-                        <Col sm={12} lg={12} className="d-none d-md-block d-lg-none centered-md">
-                        Joey Berger
-                        <div className="header-subtext-md">Software Engineer - Sound Designer - Composer - Guitarist</div>
-                        </Col>
-                        <Col xs={12} className="d-none d-sm-block d-md-none centered-sm">
-                        Joey Berger
-                        </Col>
-                        <Col xs={12} className="d-none d-xs-block d-sm-none centered-xs">
-                        Joey Berger
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <CustomNavbar scrollToDiv={this.scrollToDiv} setNavBarExpanded={this.setNavBarExpanded} expanded={this.state.expanded} categories={this.categories}/>
+            <Landing isMobile={isMobile}/>
             <div className="scrollable-container">
                 <Container fluid className="info-container white-background" id = "software" style = {{padding:`${this.containerPadding}px`}}>
                     <Row>
                         <Col md={{ span: 11, offset: 1 }}>
                             <h1>{landingPageContent.software.header}</h1>
                             <ul>
-                                {landingPageContent.software.listContent.map(l => {
-                                        if (l.includes('Guitar Mult')) {
-                                            return <li>iOS developer for personal projects and clients. <a href='https://apps.apple.com/us/app/guitar-mult/id1533512980'>Guitar Mult</a> and <a href='https://apps.apple.com/us/app/old-ekg/id1448210793'>Old EKG</a> among apps I have engineered.</li>
-                                        } else {
-                                            return <li key ={l}>{l}</li>
-                                        }                                        
-                                    }                                    
+                                {landingPageContent.software.listContent.map(l => 
+                                    l.includes('Guitar Mult') 
+                                    ? 
+                                        <li key={l}>iOS developer for personal projects and clients. <a href='https://apps.apple.com/us/app/guitar-mult/id1533512980'>Guitar Mult</a> and <a href='https://apps.apple.com/us/app/old-ekg/id1448210793'>Old EKG</a> among apps I have engineered.</li>
+                                    :
+                                        <li key ={l}>{l}</li>                                    
                                 )}
                             </ul>
                             <Container>
