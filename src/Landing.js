58e17193bf8fc3dbefdff1715887dc52a34b0c57
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2022-03-22 20:54:38 -0700

    text and imaging basics working

--- src/Landing.js
+++ src/Landing.js
@@ -5,26 +5,30 @@ import cutout from './images/landing/MainImage_Cutout.png'
 
 
 const Landing = (props) => {
+    setTimeout(() => {
+        const elem = document.getElementById('background-image')
+        // elem
+        elem.classList.remove('shimmer');
+        elem.classList.add('shimmer-idle');
+    },15000)
     return (
         <>
             <div className="image-container">
                 {/* <div className = {`background-img ${props.isMobile ? 'mobile-source' : ''} fade-in`}></div> */}
                 <div className = {`background-img ${props.isMobile ? 'mobile-source' : ''}`}>
                     <div style={{position:'fixed',height:'100%',width:'100%'}}>
-                        <img src={image} alt='test' className='img-test shimmer'></img>
-                        <img src={cutout} alt='test' className='img-test img-test-anim'></img>
-
-                        
+                        <img src={image} alt='test' className='img-test shimmer' id='background-image'></img>
+                        <img src={cutout} alt='test' className='img-test img-test-anim'></img>                        
                     </div>                     
                 </div>                
-                <Container className="centered">
+                <Container style={{marginTop:'0px'}}>
                     <Row>
-                        <Col sm={12} lg={12} className="d-none d-xl-block centered">
-                            Joey Berger
+                        <Col lg={12} className="d-none d-xl-block centered">
+                            Joey Berger sss
                         <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
                         </Col>
                         <Col sm={12} lg={12} className="d-none d-lg-block d-xl-none centered-lg">
-                            Joey Berger
+                            Joey Berger xxx
                         <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
                         </Col>
                         <Col sm={12} lg={12} className="d-none d-md-block d-lg-none centered-md">
@@ -37,6 +41,9 @@ const Landing = (props) => {
                         <Col xs={12} className="d-none d-xs-block d-sm-none centered-xs">
                             Joey Berger
                         </Col>
+                        {/* <Col xs={12} className="d-none d-sm-block d-md-none centered-sm" style={{marginLeft:'100px',fontSize:'20px'}}>
+                            Joey Berger
+                        </Col> */}
                     </Row>
                 </Container>
             </div>
