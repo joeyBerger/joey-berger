d717f26ff90c72f8818b99e19560b10629a64cd9
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2022-03-22 21:47:51 -0700

    added mobile

--- src/Landing.js
+++ src/Landing.js
@@ -18,30 +18,36 @@ const Landing = (props) => {
                 <div className = {`background-img ${props.isMobile ? 'mobile-source' : ''}`}>
                     <div style={{position:'fixed',height:'100%',width:'100%'}}>
                         <img src={image} alt='test' className='img-test shimmer' id='background-image'></img>
-                        <img src={cutout} alt='test' className='img-test img-test-anim'></img>                        
+                        <img src={cutout} alt='test' className={`img-test img-test-anim ${props.isMobile ? 'mobile-anim' : ''}`}></img>                        
                     </div>                     
                 </div>                
                 <Container className='title'>
                     <Row>
-                        <Col lg={12} className="d-none d-xl-block centered">
-                            Joey Berger sss
-                        <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
-                        </Col>
-                        <Col sm={12} className="d-none d-lg-block d-xl-none centered-lg">
-                            Joey Berger xxx
-                        <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
-                        </Col>
-                        <Col sm={12} lassName="d-none d-md-block d-lg-none centered-md">
+                        <Col xl={12} className="d-none d-xl-block centered">
                             Joey Berger
-                        <div className="header-subtext-md">Software Engineer - Sound Designer - Composer - Guitarist</div>
+                            <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
+                        </Col>
+                        <Col lg={12} className="d-none d-lg-block d-xl-none centered-lg">
+                            Joey Berger
+                            <div className="header-subtext">Software Engineer - Sound Designer - Composer - Guitarist</div>
+                        </Col>
+                        <Col sm={12} className="d-none d-md-block d-lg-none centered-md">
+                            Joey Berger
+                            <div className="header-subtext-md">Software Engineer - Sound Designer - Composer - Guitarist</div>
                         </Col>
                         <Col xs={{offset:1}} className="d-none d-sm-block d-md-none centered-sm">
+                            Joey Berger
+                        </Col>
+                        <Col xs={{offset:1}} className="d-none d-xs-block d-sm-none centered-sm">
                             Joey Berger
                         </Col>
                         {/* <Col xs={12} className="d-none d-xs-block d-sm-none centered-xs">
                             Joey Berger
                         </Col> */}
-                     </Row>
+                        {/* <Col xs={12} className="d-none d-sm-block d-md-none centered-sm" style={{marginLeft:'100px',fontSize:'20px'}}>
+                            Joey Berger
+                        </Col> */}
+                    </Row>
                 </Container>
             </div>
         </>
